b03ca53fd4d802104884595f24bebfada5d09f88
Catherine Batsoula <cbatsoula@gmail.com>
2019-03-02 21:19:03 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -25,6 +25,7 @@ function accessElementInArray(array, index){
 
 function destructivelyRemoveElementFromBeginningOfArray(array, element){
   (array.slice(1)) ;
+  return array
 }
 
 function removeElementFromBeginningOfArray(array){
