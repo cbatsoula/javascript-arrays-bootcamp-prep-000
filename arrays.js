784f314f28d618b564a7a8c3982075444f9585ab
Catherine Batsoula <cbatsoula@gmail.com>
2019-03-02 18:36:07 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,6 @@
 
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
-var cities = ["New York", "San Francisco"]
-cities = [...cities, "Philadelphia"]
-const cats = ["Milo", "Garfield"]
-const moreCats = ["Felix",...cats]
+
 
 
 var tea = ["Earl Grey", "Sage"]
