e8e805073fe16e97249b7a4b271746a3dc2c2ef5
Catherine Batsoula <cbatsoula@gmail.com>
2019-03-02 20:32:27 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,11 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 
-function addElementToBeginningOfArray(element, array){
+function addElementToBeginningOfArray(array, element){
   return [element,...array];
 }
 
-function destructivelyAddElementToBeginningOfArray(element, array){
+function destructivelyAddElementToBeginningOfArray(array, element){
   
   return array
 }
