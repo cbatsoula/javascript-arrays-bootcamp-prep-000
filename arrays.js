a1094fc67909e92072fafdf6106175a67288936f
Catherine Batsoula <cbatsoula@gmail.com>
2019-03-02 18:55:55 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,1 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
-
-
-
-var tea = ["Earl Grey", "Sage"]
-tea = new Array();
-function addElementToBeginningOfArray(tea, 3)
-var newLength = tea.unshift('Jasmine')
-
-function destructivelyAddElementToBeginningOfArray(tea, 3)
-tea.unshift("Rooiboos")
