a29479efa4bb4d0c728cc9dca6fec73123f5e562
Catherine Batsoula <cbatsoula@gmail.com>
2019-03-02 21:08:20 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -24,8 +24,9 @@ function accessElementInArray(array, index){
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array){
-  return array.slice(1);
-
+  
+array = array.slice(1) ;
+return array.slice(1);
 }
 function destructivelyRemoveElementFromEndOfArray(array, element) {
   return
