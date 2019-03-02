1031f831f5060334cd766074b90da228ca4d05fd
Catherine Batsoula <cbatsoula@gmail.com>
2019-03-02 20:43:14 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,7 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  foo.unshift(array)
+  array.unshift('foo')
   return array ;
 }
 
