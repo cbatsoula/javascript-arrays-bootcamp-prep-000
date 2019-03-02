14243a3710c2f16ec5a24579154b6a6acecc2760
Catherine Batsoula <cbatsoula@gmail.com>
2019-03-02 19:27:55 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 
 function addElementToBeginningOfArray(element, array) { 
-  console.log(array1.unshift(4, 5)) ;
+  console.log(array.unshift(4, 5)) ;
   return array ;
   }
 
